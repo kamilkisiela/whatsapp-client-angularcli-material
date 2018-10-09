Urigo/WhatsApp-Clone-Client-Angular

Step 4.2: Add generated types

src/app/chat-viewer/components/message-item/message-item.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core';
+import {GetChat} from '../../../../types';
 
 @Component({
   selector: 'app-message-item',
@@ -14,7 +15,7 @@ import {Component, Input} from '@angular/core';
 export class MessageItemComponent {
   // tslint:disable-next-line:no-input-rename
   @Input('item')
-  message: any;
+  message: GetChat.Messages;
 
   @Input()
   isGroup: boolean;

src/app/chat-viewer/components/messages-list/messages-list.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core';
+import {GetChat} from '../../../../types';
 
 @Component({
   selector: 'app-messages-list',
@@ -14,7 +15,7 @@ import {Component, Input} from '@angular/core';
 export class MessagesListComponent {
   // tslint:disable-next-line:no-input-rename
   @Input('items')
-  messages: any[];
+  messages: GetChat.Messages[];
 
   @Input()
   isGroup: boolean;

src/app/chat-viewer/containers/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ChatsService} from '../../../services/chats.service';
+import {GetChat} from '../../../../types';
 
 @Component({
   template: `
@@ -19,7 +20,7 @@ import {ChatsService} from '../../../services/chats.service';
 })
 export class ChatComponent implements OnInit {
   chatId: string;
-  messages: any[];
+  messages: GetChat.Messages[];
   name: string;
   isGroup: boolean;
 

src/app/services/chats.service.ts
@@ -3,7 +3,7 @@ import {map} from 'rxjs/operators';
 import {Apollo} from 'apollo-angular';
 import {Injectable} from '@angular/core';
 import {getChatsQuery} from '../../graphql/getChats.query';
-import {GetChats} from '../../types';
+import {GetChat, GetChats} from '../../types';
 import {getChatQuery} from '../../graphql/getChat.query';
 
 @Injectable()
@@ -23,7 +23,7 @@ export class ChatsService {
   }
 
   getChat(chatId: string) {
-    const query = this.apollo.watchQuery<any>({
+    const query = this.apollo.watchQuery<GetChat.Query>({
       query: getChatQuery,
       variables: {
         chatId: chatId,
@@ -31,7 +31,7 @@ export class ChatsService {
     });
 
     const chat$ = query.valueChanges.pipe(
-      map((result: ApolloQueryResult<any>) => result.data.chat)
+      map((result: ApolloQueryResult<GetChat.Query>) => result.data.chat)
     );
 
     return {query, chat$};

kamilkisiela/whatsapp-client-angularcli-material

src/app/chat-viewer/components/message-item/message-item.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core';
+import {GetChat} from '../../../../graphql';
 
 @Component({
   selector: 'app-message-item',
@@ -14,7 +15,7 @@ import {Component, Input} from '@angular/core';
 export class MessageItemComponent {
   // tslint:disable-next-line:no-input-rename
   @Input('item')
-  message: any;
+  message: GetChat.Messages;
 
   @Input()
   isGroup: boolean;

src/app/chat-viewer/components/messages-list/messages-list.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core';
+import {GetChat} from '../../../../graphql';
 
 @Component({
   selector: 'app-messages-list',
@@ -14,7 +15,7 @@ import {Component, Input} from '@angular/core';
 export class MessagesListComponent {
   // tslint:disable-next-line:no-input-rename
   @Input('items')
-  messages: any[];
+  messages: GetChat.Messages[];
 
   @Input()
   isGroup: boolean;

src/app/chat-viewer/containers/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ChatsService} from '../../../services/chats.service';
+import {GetChat} from '../../../../graphql';
 
 @Component({
   template: `
@@ -19,7 +20,7 @@ import {ChatsService} from '../../../services/chats.service';
 })
 export class ChatComponent implements OnInit {
   chatId: string;
-  messages: any[];
+  messages: GetChat.Messages[];
   name: string;
   isGroup: boolean;
 

src/app/services/chats.service.ts
@@ -2,7 +2,7 @@ import {map} from 'rxjs/operators';
 import {Apollo} from 'apollo-angular';
 import {Injectable} from '@angular/core';
 import {getChatsQuery} from '../../graphql/getChats.query';
-import {GetChats} from '../../graphql';
+import {GetChat, GetChats} from '../../graphql';
 import {getChatQuery} from '../../graphql/getChat.query';
 
 @Injectable()
@@ -26,7 +26,7 @@ export class ChatsService {
   }
 
   getChat(chatId: string) {
-    const query = this.apollo.watchQuery<any>({
+    const query = this.apollo.watchQuery<GetChat.Query>({
       query: getChatQuery,
       variables: {
         chatId: chatId,
@@ -34,7 +34,7 @@ export class ChatsService {
     });
 
     const chat$ = query.valueChanges.pipe(
-      map((result: ApolloQueryResult<any>) => result.data.chat)
+      map((result: ApolloQueryResult<GetChat.Query>) => result.data.chat)
     );
 
     return {query, chat$};