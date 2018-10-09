Urigo/WhatsApp-Clone-Client-Angular

Step 8.1: New chats and groups

src/app/chats-creation/chats-creation.module.ts
@@ -1,0 +1,60 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatButtonModule, MatFormFieldModule, MatGridListModule, MatIconModule, MatInputModule, MatListModule, MatMenuModule,
+  MatToolbarModule
+} from '@angular/material';
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {ChatsService} from '../services/chats.service';
+import {UserItemComponent} from './components/user-item/user-item.component';
+import {UsersListComponent} from './components/users-list/users-list.component';
+import {NewGroupComponent} from './containers/new-group/new-group.component';
+import {NewChatComponent} from './containers/new-chat/new-chat.component';
+import {NewGroupDetailsComponent} from './components/new-group-details/new-group-details.component';
+import {SharedModule} from '../shared/shared.module';
+import {SelectableListModule} from 'ngx-selectable-list';
+
+const routes: Routes = [
+  {path: 'new-chat', component: NewChatComponent},
+  {path: 'new-group', component: NewGroupComponent},
+];
+
+@NgModule({
+  declarations: [
+    NewChatComponent,
+    UsersListComponent,
+    NewGroupComponent,
+    UserItemComponent,
+    NewGroupDetailsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    // Animations (for Material)
+    BrowserAnimationsModule,
+    // Material
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    MatGridListModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    // Routing
+    RouterModule.forChild(routes),
+    // Forms
+    FormsModule,
+    // Feature modules
+    SelectableListModule,
+    SharedModule,
+  ],
+  providers: [
+    ChatsService,
+  ],
+})
+export class ChatsCreationModule {
+}

src/app/chats-creation/components/new-group-details/new-group-details.component.ts
@@ -1,0 +1,34 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {GetUsers} from '../../../../types';
+
+@Component({
+  selector: 'app-new-group-details',
+  template: `
+    <div>
+      <mat-form-field>
+        <input matInput placeholder="Group name" [(ngModel)]="groupName">
+      </mat-form-field>
+    </div>
+    <button [disabled]="!groupName" class="new-group" mat-fab color="primary" (click)="emitGroupDetails()">
+      <mat-icon aria-label="Icon-button with a + icon">arrow_forward</mat-icon>
+    </button>
+    <div>Members</div>
+    <div class="users">
+      <img *ngFor="let user of users;" [src]="user.picture"/>
+    </div>
+  `,
+  styleUrls: ['new-group-details.component.scss'],
+})
+export class NewGroupDetailsComponent {
+  groupName: string;
+  @Input()
+  users: GetUsers.Users[];
+  @Output()
+  groupDetails = new EventEmitter<string>();
+
+  emitGroupDetails() {
+    if (this.groupDetails) {
+      this.groupDetails.emit(this.groupName);
+    }
+  }
+}

src/app/chats-creation/components/user-item/user-item.component.ts
@@ -1,0 +1,20 @@
+import {Component, Input} from '@angular/core';
+import {GetUsers} from '../../../../types';
+
+@Component({
+  selector: 'app-user-item',
+  template: `
+    <button mat-menu-item>
+      <div>
+        <img [src]="user.picture" *ngIf="user.picture">
+      </div>
+      <div>{{ user.name }}</div>
+    </button>
+  `,
+  styleUrls: ['user-item.component.scss']
+})
+export class UserItemComponent {
+  // tslint:disable-next-line:no-input-rename
+  @Input('item')
+  user: GetUsers.Users;
+}

src/app/chats-creation/components/users-list/users-list.component.ts
@@ -1,0 +1,24 @@
+import {Component, Input} from '@angular/core';
+import {GetUsers} from '../../../../types';
+import {SelectableListDirective} from 'ngx-selectable-list';
+
+@Component({
+  selector: 'app-users-list',
+  template: `
+    <mat-list>
+      <mat-list-item *ngFor="let user of users">
+        <app-user-item [item]="user"
+                       appSelectableItem></app-user-item>
+      </mat-list-item>
+    </mat-list>
+    <ng-content *ngIf="selectableListDirective.selecting"></ng-content>
+  `,
+  styleUrls: ['users-list.component.scss'],
+})
+export class UsersListComponent {
+  // tslint:disable-next-line:no-input-rename
+  @Input('items')
+  users: GetUsers.Users[];
+
+  constructor(public selectableListDirective: SelectableListDirective) {}
+}

src/app/chats-creation/containers/new-chat/new-chat.component.ts
@@ -1,0 +1,59 @@
+import {Component, OnInit} from '@angular/core';
+import {Location} from '@angular/common';
+import {Router} from '@angular/router';
+import {AddChat, GetUsers} from '../../../../types';
+import {ChatsService} from '../../../services/chats.service';
+
+@Component({
+  template: `
+    <app-toolbar>
+      <button class="navigation" mat-button (click)="goBack()">
+        <mat-icon aria-label="Icon-button with an arrow back icon">arrow_back</mat-icon>
+      </button>
+      <div class="title">New chat</div>
+    </app-toolbar>
+
+    <div class="new-group" (click)="goToNewGroup()">
+      <div>
+        <mat-icon aria-label="Icon-button with a group add icon">group_add</mat-icon>
+      </div>
+      <div>New group</div>
+    </div>
+
+    <app-users-list [items]="users"
+                    appSelectableList="single" (single)="addChat($event)">
+    </app-users-list>
+  `,
+  styleUrls: ['new-chat.component.scss'],
+})
+export class NewChatComponent implements OnInit {
+  users: GetUsers.Users[];
+
+  constructor(private router: Router,
+              private location: Location,
+              private chatsService: ChatsService) {}
+
+  ngOnInit () {
+    this.chatsService.getUsers().users$.subscribe(users => this.users = users);
+  }
+
+  goBack() {
+    this.location.back();
+  }
+
+  goToNewGroup() {
+    this.router.navigate(['/new-group']);
+  }
+
+  addChat(recipientId: string) {
+    const chatId = this.chatsService.getChatId(recipientId);
+    if (chatId) {
+      // Chat is already listed for the current user
+      this.router.navigate(['/chat', chatId]);
+    } else {
+      this.chatsService.addChat(recipientId).subscribe(({data: {addChat: {id}}}: { data: AddChat.Mutation }) => {
+        this.router.navigate(['/chat', id]);
+      });
+    }
+  }
+}

src/app/chats-creation/containers/new-group/new-group.component.ts
@@ -1,0 +1,60 @@
+import {Component, OnInit} from '@angular/core';
+import {Location} from '@angular/common';
+import {Router} from '@angular/router';
+import {AddGroup, GetUsers} from '../../../../types';
+import {ChatsService} from '../../../services/chats.service';
+
+@Component({
+  template: `
+    <app-toolbar>
+      <button class="navigation" mat-button (click)="goBack()">
+        <mat-icon aria-label="Icon-button with an arrow back icon">arrow_back</mat-icon>
+      </button>
+      <div class="title">New group</div>
+    </app-toolbar>
+
+    <app-users-list *ngIf="!recipientIds.length" [items]="users"
+                    appSelectableList="multiple_tap" (multiple)="selectUsers($event)">
+      <app-confirm-selection #confirmSelection icon="arrow_forward"></app-confirm-selection>
+    </app-users-list>
+    <app-new-group-details *ngIf="recipientIds.length" [users]="getSelectedUsers()"
+                           (groupDetails)="addGroup($event)"></app-new-group-details>
+  `,
+  styleUrls: ['new-group.component.scss'],
+})
+export class NewGroupComponent implements OnInit {
+  users: GetUsers.Users[];
+  recipientIds: string[] = [];
+
+  constructor(private router: Router,
+              private location: Location,
+              private chatsService: ChatsService) {}
+
+  ngOnInit () {
+    this.chatsService.getUsers().users$.subscribe(users => this.users = users);
+  }
+
+  goBack() {
+    if (this.recipientIds.length) {
+      this.recipientIds = [];
+    } else {
+      this.location.back();
+    }
+  }
+
+  selectUsers(recipientIds: string[]) {
+    this.recipientIds = recipientIds;
+  }
+
+  getSelectedUsers() {
+    return this.users.filter(user => this.recipientIds.includes(user.id));
+  }
+
+  addGroup(groupName: string) {
+    if (groupName && this.recipientIds.length) {
+      this.chatsService.addGroup(this.recipientIds, groupName).subscribe(({data: {addGroup: {id}}}: { data: AddGroup.Mutation }) => {
+        this.router.navigate(['/chat', id]);
+      });
+    }
+  }
+}

src/app/services/chats.service.ts
@@ -1,30 +1,40 @@
 import {ApolloQueryResult} from 'apollo-client';
 import {map} from 'rxjs/operators';
-import {Apollo} from 'apollo-angular';
+import {Apollo, QueryRef} from 'apollo-angular';
 import {Injectable} from '@angular/core';
 import {getChatsQuery} from '../../graphql/getChats.query';
-import {AddMessage, GetChat, GetChats, RemoveAllMessages, RemoveChat, RemoveMessages} from '../../types';
+import {AddChat, AddGroup, AddMessage, GetChat, GetChats, GetUsers, RemoveAllMessages, RemoveChat, RemoveMessages} from '../../types';
 import {getChatQuery} from '../../graphql/getChat.query';
 import {addMessageMutation} from '../../graphql/addMessage.mutation';
 import {removeChatMutation} from '../../graphql/removeChat.mutation';
 import {DocumentNode} from 'graphql';
 import {removeAllMessagesMutation} from '../../graphql/removeAllMessages.mutation';
 import {removeMessagesMutation} from '../../graphql/removeMessages.mutation';
+import {getUsersQuery} from '../../graphql/getUsers.query';
+import {Observable} from 'rxjs/Observable';
+import {addChatMutation} from '../../graphql/addChat.mutation';
+import {addGroupMutation} from '../../graphql/addGroup.mutation';
+
+const currentUserId = '1';
 
 @Injectable()
 export class ChatsService {
+  getChatsWq: QueryRef<GetChats.Query>;
+  chats$: Observable<GetChats.Chats[]>;
+  chats: GetChats.Chats[];
 
-  constructor(private apollo: Apollo) {}
-
-  getChats() {
-    const query = this.apollo.watchQuery<GetChats.Query>({
+  constructor(private apollo: Apollo) {
+    this.getChatsWq = this.apollo.watchQuery<GetChats.Query>({
       query: getChatsQuery
     });
-    const chats$ = query.valueChanges.pipe(
+    this.chats$ = this.getChatsWq.valueChanges.pipe(
       map((result: ApolloQueryResult<GetChats.Query>) => result.data.chats)
     );
+    this.chats$.subscribe(chats => this.chats = chats);
+  }
 
-    return {query, chats$};
+  getChats() {
+    return {query: this.getChatsWq, chats$: this.chats$};
   }
 
   getChat(chatId: string) {
@@ -146,6 +156,60 @@ export class ChatsService {
           // Write our data back to the cache.
           store.writeQuery({ query: getChatsQuery, data: {chats} });
         }
+      },
+    });
+  }
+
+  getUsers() {
+    const query = this.apollo.watchQuery<GetUsers.Query>({
+      query: getUsersQuery,
+    });
+    const users$ = query.valueChanges.pipe(
+      map((result: ApolloQueryResult<GetUsers.Query>) => result.data.users)
+    );
+
+    return {query, users$};
+  }
+
+  // Checks if the chat is listed for the current user and returns the id
+  getChatId(recipientId: string) {
+    const _chat = this.chats.find(chat => {
+      return !chat.isGroup && chat.userIds.includes(currentUserId) && chat.userIds.includes(recipientId);
+    });
+    return _chat ? _chat.id : false;
+  }
+
+  addChat(recipientId: string) {
+    return this.apollo.mutate({
+      mutation: addChatMutation,
+      variables: <AddChat.Variables>{
+        recipientId,
+      },
+      update: (store, { data: { addChat } }) => {
+        // Read the data from our cache for this query.
+        const {chats}: GetChats.Query = store.readQuery({ query: getChatsQuery });
+        // Add our comment from the mutation to the end.
+        chats.push(addChat);
+        // Write our data back to the cache.
+        store.writeQuery({ query: getChatsQuery, data: {chats} });
+      },
+    });
+  }
+
+  addGroup(recipientIds: string[], groupName: string) {
+    return this.apollo.mutate({
+      mutation: addGroupMutation,
+      variables: <AddGroup.Variables>{
+        recipientIds,
+        groupName,
+      },
+      update: (store, { data: { addGroup } }) => {
+        // Read the data from our cache for this query.
+        const {chats}: GetChats.Query = store.readQuery({ query: getChatsQuery });
+        // Add our comment from the mutation to the end.
+        chats.push(addGroup);
+        // Write our data back to the cache.
+        store.writeQuery({ query: getChatsQuery, data: {chats} });
       },
     });
   }

kamilkisiela/whatsapp-client-angularcli-material

src/app/chats-creation/chats-creation.module.ts
@@ -1,0 +1,60 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatButtonModule, MatFormFieldModule, MatGridListModule, MatIconModule, MatInputModule, MatListModule, MatMenuModule,
+  MatToolbarModule
+} from '@angular/material';
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {ChatsService} from '../services/chats.service';
+import {UserItemComponent} from './components/user-item/user-item.component';
+import {UsersListComponent} from './components/users-list/users-list.component';
+import {NewGroupComponent} from './containers/new-group/new-group.component';
+import {NewChatComponent} from './containers/new-chat/new-chat.component';
+import {NewGroupDetailsComponent} from './components/new-group-details/new-group-details.component';
+import {SharedModule} from '../shared/shared.module';
+import {NgxSelectableListModule} from 'ngx-selectable-list';
+
+const routes: Routes = [
+  {path: 'new-chat', component: NewChatComponent},
+  {path: 'new-group', component: NewGroupComponent},
+];
+
+@NgModule({
+  declarations: [
+    NewChatComponent,
+    UsersListComponent,
+    NewGroupComponent,
+    UserItemComponent,
+    NewGroupDetailsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    // Animations (for Material)
+    BrowserAnimationsModule,
+    // Material
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    MatGridListModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    // Routing
+    RouterModule.forChild(routes),
+    // Forms
+    FormsModule,
+    // Feature modules
+    NgxSelectableListModule,
+    SharedModule,
+  ],
+  providers: [
+    ChatsService,
+  ],
+})
+export class ChatsCreationModule {
+}

src/app/chats-creation/components/new-group-details/new-group-details.component.ts
@@ -1,0 +1,34 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {GetUsers} from '../../../../graphql';
+
+@Component({
+  selector: 'app-new-group-details',
+  template: `
+    <div>
+      <mat-form-field>
+        <input matInput placeholder="Group name" [(ngModel)]="groupName">
+      </mat-form-field>
+    </div>
+    <button [disabled]="!groupName" class="new-group" mat-fab color="primary" (click)="emitGroupDetails()">
+      <mat-icon aria-label="Icon-button with a + icon">arrow_forward</mat-icon>
+    </button>
+    <div>Members</div>
+    <div class="users">
+      <img *ngFor="let user of users;" [src]="user.picture"/>
+    </div>
+  `,
+  styleUrls: ['new-group-details.component.scss'],
+})
+export class NewGroupDetailsComponent {
+  groupName: string;
+  @Input()
+  users: GetUsers.Users[];
+  @Output()
+  groupDetails = new EventEmitter<string>();
+
+  emitGroupDetails() {
+    if (this.groupDetails) {
+      this.groupDetails.emit(this.groupName);
+    }
+  }
+}

src/app/chats-creation/components/user-item/user-item.component.ts
@@ -1,0 +1,20 @@
+import {Component, Input} from '@angular/core';
+import {GetUsers} from '../../../../graphql';
+
+@Component({
+  selector: 'app-user-item',
+  template: `
+    <button mat-menu-item>
+      <div>
+        <img [src]="user.picture" *ngIf="user.picture">
+      </div>
+      <div>{{ user.name }}</div>
+    </button>
+  `,
+  styleUrls: ['user-item.component.scss']
+})
+export class UserItemComponent {
+  // tslint:disable-next-line:no-input-rename
+  @Input('item')
+  user: GetUsers.Users;
+}

src/app/chats-creation/components/users-list/users-list.component.ts
@@ -1,0 +1,24 @@
+import {Component, Input} from '@angular/core';
+import {GetUsers} from '../../../../graphql';
+import {SelectableListDirective} from 'ngx-selectable-list';
+
+@Component({
+  selector: 'app-users-list',
+  template: `
+    <mat-list>
+      <mat-list-item *ngFor="let user of users">
+        <app-user-item [item]="user"
+                       libSelectableItem></app-user-item>
+      </mat-list-item>
+    </mat-list>
+    <ng-content *ngIf="selectableListDirective.selecting"></ng-content>
+  `,
+  styleUrls: ['users-list.component.scss'],
+})
+export class UsersListComponent {
+  // tslint:disable-next-line:no-input-rename
+  @Input('items')
+  users: GetUsers.Users[];
+
+  constructor(public selectableListDirective: SelectableListDirective) {}
+}

src/app/chats-creation/containers/new-chat/new-chat.component.ts
@@ -1,0 +1,59 @@
+import {Component, OnInit} from '@angular/core';
+import {Location} from '@angular/common';
+import {Router} from '@angular/router';
+import {AddChat, GetUsers} from '../../../../graphql';
+import {ChatsService} from '../../../services/chats.service';
+
+@Component({
+  template: `
+    <app-toolbar>
+      <button class="navigation" mat-button (click)="goBack()">
+        <mat-icon aria-label="Icon-button with an arrow back icon">arrow_back</mat-icon>
+      </button>
+      <div class="title">New chat</div>
+    </app-toolbar>
+
+    <div class="new-group" (click)="goToNewGroup()">
+      <div>
+        <mat-icon aria-label="Icon-button with a group add icon">group_add</mat-icon>
+      </div>
+      <div>New group</div>
+    </div>
+
+    <app-users-list [items]="users"
+                    libSelectableList="single" (single)="addChat($event)">
+    </app-users-list>
+  `,
+  styleUrls: ['new-chat.component.scss'],
+})
+export class NewChatComponent implements OnInit {
+  users: GetUsers.Users[];
+
+  constructor(private router: Router,
+              private location: Location,
+              private chatsService: ChatsService) {}
+
+  ngOnInit () {
+    this.chatsService.getUsers().users$.subscribe(users => this.users = users);
+  }
+
+  goBack() {
+    this.location.back();
+  }
+
+  goToNewGroup() {
+    this.router.navigate(['/new-group']);
+  }
+
+  addChat(recipientId: string) {
+    const chatId = this.chatsService.getChatId(recipientId);
+    if (chatId) {
+      // Chat is already listed for the current user
+      this.router.navigate(['/chat', chatId]);
+    } else {
+      this.chatsService.addChat(recipientId).subscribe(({data: {addChat: {id}}}: { data: AddChat.Mutation }) => {
+        this.router.navigate(['/chat', id]);
+      });
+    }
+  }
+}

src/app/chats-creation/containers/new-group/new-group.component.ts
@@ -1,0 +1,60 @@
+import {Component, OnInit} from '@angular/core';
+import {Location} from '@angular/common';
+import {Router} from '@angular/router';
+import {AddGroup, GetUsers} from '../../../../graphql';
+import {ChatsService} from '../../../services/chats.service';
+
+@Component({
+  template: `
+    <app-toolbar>
+      <button class="navigation" mat-button (click)="goBack()">
+        <mat-icon aria-label="Icon-button with an arrow back icon">arrow_back</mat-icon>
+      </button>
+      <div class="title">New group</div>
+    </app-toolbar>
+
+    <app-users-list *ngIf="!recipientIds.length" [items]="users"
+                    libSelectableList="multiple_tap" (multiple)="selectUsers($event)">
+      <app-confirm-selection #confirmSelection icon="arrow_forward"></app-confirm-selection>
+    </app-users-list>
+    <app-new-group-details *ngIf="recipientIds.length" [users]="getSelectedUsers()"
+                           (groupDetails)="addGroup($event)"></app-new-group-details>
+  `,
+  styleUrls: ['new-group.component.scss'],
+})
+export class NewGroupComponent implements OnInit {
+  users: GetUsers.Users[];
+  recipientIds: string[] = [];
+
+  constructor(private router: Router,
+              private location: Location,
+              private chatsService: ChatsService) {}
+
+  ngOnInit () {
+    this.chatsService.getUsers().users$.subscribe(users => this.users = users);
+  }
+
+  goBack() {
+    if (this.recipientIds.length) {
+      this.recipientIds = [];
+    } else {
+      this.location.back();
+    }
+  }
+
+  selectUsers(recipientIds: string[]) {
+    this.recipientIds = recipientIds;
+  }
+
+  getSelectedUsers() {
+    return this.users.filter(user => this.recipientIds.includes(user.id));
+  }
+
+  addGroup(groupName: string) {
+    if (groupName && this.recipientIds.length) {
+      this.chatsService.addGroup(this.recipientIds, groupName).subscribe(({data: {addGroup: {id}}}: { data: AddGroup.Mutation }) => {
+        this.router.navigate(['/chat', id]);
+      });
+    }
+  }
+}

src/app/services/chats.service.ts
@@ -1,37 +1,47 @@
 import {map} from 'rxjs/operators';
-import {Apollo} from 'apollo-angular';
+import {Apollo, QueryRef} from 'apollo-angular';
 import {Injectable} from '@angular/core';
 import {getChatsQuery} from '../../graphql/getChats.query';
-import {AddMessage, GetChat, GetChats, RemoveAllMessages, RemoveChat, RemoveMessages} from '../../graphql';
+import {AddChat, AddGroup, AddMessage, GetChat, GetChats, GetUsers, RemoveAllMessages, RemoveChat, RemoveMessages} from '../../graphql';
 import {getChatQuery} from '../../graphql/getChat.query';
 import {addMessageMutation} from '../../graphql/addMessage.mutation';
 import {removeChatMutation} from '../../graphql/removeChat.mutation';
 import {DocumentNode} from 'graphql';
 import {removeAllMessagesMutation} from '../../graphql/removeAllMessages.mutation';
 import {removeMessagesMutation} from '../../graphql/removeMessages.mutation';
+import {getUsersQuery} from '../../graphql/getUsers.query';
+import {Observable} from 'rxjs';
+import {addChatMutation} from '../../graphql/addChat.mutation';
+import {addGroupMutation} from '../../graphql/addGroup.mutation';
+
+const currentUserId = '1';
 
 @Injectable()
 export class ChatsService {
   messagesAmount = 3;
+  getChatsWq: QueryRef<GetChats.Query, GetChats.Variables>;
+  chats$: Observable<GetChats.Chats[]>;
+  chats: GetChats.Chats[];
 
-  constructor(private apollo: Apollo) {}
-
-  getChats() {
-    const query = this.apollo.watchQuery<GetChats.Query>({
+  constructor(private apollo: Apollo) {
+    this.getChatsWq = this.apollo.watchQuery<GetChats.Query, GetChats.Variables>({
       query: getChatsQuery,
       variables: {
         amount: this.messagesAmount,
       },
     });
-    const chats$ = query.valueChanges.pipe(
+    this.chats$ = this.getChatsWq.valueChanges.pipe(
       map((result) => result.data.chats)
     );
+    this.chats$.subscribe(chats => this.chats = chats);
+  }
 
-    return {query, chats$};
+  getChats() {
+    return {query: this.getChatsWq, chats$: this.chats$};
   }
 
   getChat(chatId: string) {
-    const query = this.apollo.watchQuery<GetChat.Query>({
+    const query = this.apollo.watchQuery<GetChat.Query, GetChat.Variables>({
       query: getChatQuery,
       variables: {
         chatId: chatId,
@@ -141,7 +151,10 @@ export class ChatsService {
       mutation = removeMessagesMutation;
     }
 
-    return this.apollo.mutate({
+    return this.apollo.mutate<
+      RemoveMessages.Mutation | RemoveAllMessages.Mutation,
+      RemoveMessages.Variables | RemoveAllMessages.Variables
+    >({
       mutation,
       variables,
       update: (store, { data: { removeMessages } }: {data: RemoveMessages.Mutation | RemoveAllMessages.Mutation}) => {
@@ -188,6 +201,87 @@ export class ChatsService {
             },
           });
         }
+      },
+    });
+  }
+
+  getUsers() {
+    const query = this.apollo.watchQuery<GetUsers.Query, GetUsers.Variables>({
+      query: getUsersQuery,
+    });
+    const users$ = query.valueChanges.pipe(
+      map((result) => result.data.users)
+    );
+
+    return {query, users$};
+  }
+
+  // Checks if the chat is listed for the current user and returns the id
+  getChatId(recipientId: string) {
+    const _chat = this.chats.find(chat => {
+      return !chat.isGroup && !!chat.allTimeMembers.find(user => user.id === currentUserId) &&
+        !!chat.allTimeMembers.find(user => user.id === recipientId);
+    });
+    return _chat ? _chat.id : false;
+  }
+
+  addChat(recipientId: string) {
+    return this.apollo.mutate<AddChat.Mutation, AddChat.Variables>({
+      mutation: addChatMutation,
+      variables: {
+        recipientId,
+      },
+      update: (store, { data: { addChat } }) => {
+        // Read the data from our cache for this query.
+        const {chats} = store.readQuery<GetChats.Query, GetChats.Variables>({
+          query: getChatsQuery,
+          variables: {
+            amount: this.messagesAmount,
+          },
+        });
+        // Add our comment from the mutation to the end.
+        chats.push(addChat);
+        // Write our data back to the cache.
+        store.writeQuery<GetChats.Query, GetChats.Variables>({
+          query: getChatsQuery,
+          variables: {
+            amount: this.messagesAmount,
+          },
+          data: {
+            chats,
+          },
+        });
+      },
+    });
+  }
+
+  addGroup(recipientIds: string[], groupName: string) {
+    return this.apollo.mutate<AddGroup.Mutation, AddGroup.Variables>({
+      mutation: addGroupMutation,
+      variables: {
+        recipientIds,
+        groupName,
+      },
+      update: (store, { data: { addGroup } }) => {
+        // Read the data from our cache for this query.
+        const {chats} = store.readQuery<GetChats.Query, GetChats.Variables>({
+          query: getChatsQuery,
+          variables: {
+            amount: this.messagesAmount,
+          },
+        });
+        // Add our comment from the mutation to the end.
+        chats.push(addGroup);
+        // Write our data back to the cache.
+        store.writeQuery<GetChats.Query, GetChats.Variables>({
+          query: getChatsQuery,
+          variables: {
+            amount: this.messagesAmount,
+          },
+          data: {
+            chats,
+          },
+        });
       },
     });
   }