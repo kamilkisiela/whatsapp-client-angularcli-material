Urigo/WhatsApp-Clone-Client-Angular

Step 7.2: Remove messages and chats

src/app/chat-viewer/containers/chat/chat.component.ts
@@ -12,7 +12,10 @@ import {GetChat} from '../../../../types';
       <div class="title">{{ name }}</div>
     </app-toolbar>
     <div class="container">
-      <app-messages-list [items]="messages" [isGroup]="isGroup"></app-messages-list>
+      <app-messages-list [items]="messages" [isGroup]="isGroup"
+                         appSelectableList="multiple_press" (multiple)="deleteMessages($event)">
+        <app-confirm-selection #confirmSelection></app-confirm-selection>
+      </app-messages-list>
       <app-new-message (newMessage)="addMessage($event)"></app-new-message>
     </div>
   `,
@@ -46,5 +49,9 @@ export class ChatComponent implements OnInit {
 
   addMessage(content: string) {
     this.chatsService.addMessage(this.chatId, content).subscribe();
+  }
+
+  deleteMessages(messageIds: string[]) {
+    this.chatsService.removeMessages(this.chatId, this.messages, messageIds).subscribe();
   }
 }

src/app/chats-lister/containers/chats/chats.component.ts
@@ -28,9 +28,13 @@ import {Router} from '@angular/router';
       </button>
     </mat-menu>
 
-    <app-chats-list [items]="chats$ | async" (select)="goToChat($event)"></app-chats-list>
+    <app-chats-list [items]="chats$ | async"
+                    appSelectableList="both"
+                    (single)="goToChat($event)" (multiple)="deleteChats($event)" (isSelecting)="isSelecting = $event">
+      <app-confirm-selection #confirmSelection></app-confirm-selection>
+    </app-chats-list>
 
-    <button class="chat-button" mat-fab color="primary">
+    <button *ngIf="!isSelecting" class="chat-button" mat-fab color="primary">
       <mat-icon aria-label="Icon-button with a + icon">add</mat-icon>
     </button>
   `,
@@ -38,6 +42,7 @@ import {Router} from '@angular/router';
 })
 export class ChatsComponent implements OnInit {
   chats$: Observable<GetChats.Chats[]>;
+  isSelecting = false;
 
   constructor(private chatsService: ChatsService,
               private router: Router) {
@@ -49,5 +54,11 @@ export class ChatsComponent implements OnInit {
 
   goToChat(chatId: string) {
     this.router.navigate(['/chat', chatId]);
+  }
+
+  deleteChats(chatIds: string[]) {
+    chatIds.forEach(chatId => {
+      this.chatsService.removeChat(chatId).subscribe();
+    });
   }
 }

src/app/services/chats.service.ts
@@ -3,9 +3,13 @@ import {map} from 'rxjs/operators';
 import {Apollo} from 'apollo-angular';
 import {Injectable} from '@angular/core';
 import {getChatsQuery} from '../../graphql/getChats.query';
-import {AddMessage, GetChat, GetChats} from '../../types';
+import {AddMessage, GetChat, GetChats, RemoveAllMessages, RemoveChat, RemoveMessages} from '../../types';
 import {getChatQuery} from '../../graphql/getChat.query';
 import {addMessageMutation} from '../../graphql/addMessage.mutation';
+import {removeChatMutation} from '../../graphql/removeChat.mutation';
+import {DocumentNode} from 'graphql';
+import {removeAllMessagesMutation} from '../../graphql/removeAllMessages.mutation';
+import {removeMessagesMutation} from '../../graphql/removeMessages.mutation';
 
 @Injectable()
 export class ChatsService {
@@ -65,6 +69,80 @@ export class ChatsService {
           const {chats}: GetChats.Query = store.readQuery({ query: getChatsQuery });
           // Add our comment from the mutation to the end.
           chats.find(chat => chat.id === chatId).lastMessage = addMessage;
+          // Write our data back to the cache.
+          store.writeQuery({ query: getChatsQuery, data: {chats} });
+        }
+      },
+    });
+  }
+
+  removeChat(chatId: string) {
+    return this.apollo.mutate({
+      mutation: removeChatMutation,
+      variables: <RemoveChat.Variables>{
+        chatId,
+      },
+      update: (store, { data: { removeChat } }) => {
+        // Read the data from our cache for this query.
+        const {chats}: GetChats.Query = store.readQuery({ query: getChatsQuery });
+        // Remove the chat (mutable)
+        for (const index of chats.keys()) {
+          if (chats[index].id === removeChat) {
+            chats.splice(index, 1);
+          }
+        }
+        // Write our data back to the cache.
+        store.writeQuery({ query: getChatsQuery, data: {chats} });
+      },
+    });
+  }
+
+  removeMessages(chatId: string, messages: GetChat.Messages[], messageIdsOrAll: string[] | boolean) {
+    let variables: RemoveMessages.Variables | RemoveAllMessages.Variables;
+    let ids: string[];
+    let mutation: DocumentNode;
+
+    if (typeof messageIdsOrAll === 'boolean') {
+      variables = {chatId, all: messageIdsOrAll};
+      ids = messages.map(message => message.id);
+      mutation = removeAllMessagesMutation;
+    } else {
+      variables = {chatId, messageIds: messageIdsOrAll};
+      ids = messageIdsOrAll;
+      mutation = removeMessagesMutation;
+    }
+
+    return this.apollo.mutate({
+      mutation,
+      variables,
+      update: (store, { data: { removeMessages } }: {data: RemoveMessages.Mutation | RemoveAllMessages.Mutation}) => {
+        // Update the messages cache
+        {
+          // Read the data from our cache for this query.
+          const {chat}: GetChat.Query = store.readQuery({
+            query: getChatQuery, variables: {
+              chatId,
+            }
+          });
+          // Remove the messages (mutable)
+          removeMessages.forEach(messageId => {
+            for (const index of chat.messages.keys()) {
+              if (chat.messages[index].id === messageId) {
+                chat.messages.splice(index, 1);
+              }
+            }
+          });
+          // Write our data back to the cache.
+          store.writeQuery({ query: getChatQuery, data: {chat} });
+        }
+        // Update last message cache
+        {
+          // Read the data from our cache for this query.
+          const {chats}: GetChats.Query = store.readQuery({ query: getChatsQuery });
+          // Fix last comment
+          chats.find(chat => chat.id === chatId).lastMessage = messages
+            .filter(message => !ids.includes(message.id))
+            .sort((a, b) => b.createdAt - a.createdAt)[0] || null;
           // Write our data back to the cache.
           store.writeQuery({ query: getChatsQuery, data: {chats} });
         }

kamilkisiela/whatsapp-client-angularcli-material

src/app/chat-viewer/containers/chat/chat.component.ts
@@ -12,7 +12,10 @@ import {GetChat} from '../../../../graphql';
       <div class="title">{{ name }}</div>
     </app-toolbar>
     <div class="container">
-      <app-messages-list [items]="messages" [isGroup]="isGroup"></app-messages-list>
+      <app-messages-list [items]="messages" [isGroup]="isGroup"
+                         libSelectableList="multiple_press" (multiple)="deleteMessages($event)">
+        <app-confirm-selection #confirmSelection></app-confirm-selection>
+      </app-messages-list>
       <app-new-message (newMessage)="addMessage($event)"></app-new-message>
     </div>
   `,
@@ -46,5 +49,9 @@ export class ChatComponent implements OnInit {
 
   addMessage(content: string) {
     this.chatsService.addMessage(this.chatId, content).subscribe();
+  }
+
+  deleteMessages(messageIds: string[]) {
+    this.chatsService.removeMessages(this.chatId, this.messages, messageIds).subscribe();
   }
 }

src/app/chats-lister/containers/chats/chats.component.ts
@@ -28,9 +28,13 @@ import {Router} from '@angular/router';
       </button>
     </mat-menu>
 
-    <app-chats-list [items]="chats$ | async" (select)="goToChat($event)"></app-chats-list>
+    <app-chats-list [items]="chats$ | async"
+                    libSelectableList="both"
+                    (single)="goToChat($event)" (multiple)="deleteChats($event)" (isSelecting)="isSelecting = $event">
+      <app-confirm-selection #confirmSelection></app-confirm-selection>
+    </app-chats-list>
 
-    <button class="chat-button" mat-fab color="primary">
+    <button *ngIf="!isSelecting" class="chat-button" mat-fab color="primary">
       <mat-icon aria-label="Icon-button with a + icon">add</mat-icon>
     </button>
   `,
@@ -38,6 +42,7 @@ import {Router} from '@angular/router';
 })
 export class ChatsComponent implements OnInit {
   chats$: Observable<GetChats.Chats[]>;
+  isSelecting = false;
 
   constructor(private chatsService: ChatsService,
               private router: Router) {
@@ -49,5 +54,11 @@ export class ChatsComponent implements OnInit {
 
   goToChat(chatId: string) {
     this.router.navigate(['/chat', chatId]);
+  }
+
+  deleteChats(chatIds: string[]) {
+    chatIds.forEach(chatId => {
+      this.chatsService.removeChat(chatId).subscribe();
+    });
   }
 }

src/app/services/chats.service.ts
@@ -2,9 +2,13 @@ import {map} from 'rxjs/operators';
 import {Apollo} from 'apollo-angular';
 import {Injectable} from '@angular/core';
 import {getChatsQuery} from '../../graphql/getChats.query';
-import {AddMessage, GetChat, GetChats} from '../../graphql';
+import {AddMessage, GetChat, GetChats, RemoveAllMessages, RemoveChat, RemoveMessages} from '../../graphql';
 import {getChatQuery} from '../../graphql/getChat.query';
 import {addMessageMutation} from '../../graphql/addMessage.mutation';
+import {removeChatMutation} from '../../graphql/removeChat.mutation';
+import {DocumentNode} from 'graphql';
+import {removeAllMessagesMutation} from '../../graphql/removeAllMessages.mutation';
+import {removeMessagesMutation} from '../../graphql/removeMessages.mutation';
 
 @Injectable()
 export class ChatsService {
@@ -73,6 +77,106 @@ export class ChatsService {
           });
           // Add our comment from the mutation to the end.
           chats.find(chat => chat.id === chatId).messages.push(addMessage);
+          // Write our data back to the cache.
+          store.writeQuery<GetChats.Query, GetChats.Variables>({
+            query: getChatsQuery,
+            variables: {
+              amount: this.messagesAmount,
+            },
+            data: {
+              chats,
+            },
+          });
+        }
+      },
+    });
+  }
+
+  removeChat(chatId: string) {
+    return this.apollo.mutate<RemoveChat.Mutation, RemoveChat.Variables>({
+      mutation: removeChatMutation,
+      variables: {
+        chatId,
+      },
+      update: (store, { data: { removeChat } }) => {
+        // Read the data from our cache for this query.
+        const {chats} = store.readQuery<GetChats.Query, GetChats.Variables>({
+          query: getChatsQuery,
+          variables: {
+            amount: this.messagesAmount,
+          },
+        });
+        // Remove the chat (mutable)
+        for (const index of chats.keys()) {
+          if (chats[index].id === removeChat) {
+            chats.splice(index, 1);
+          }
+        }
+        // Write our data back to the cache.
+        store.writeQuery<GetChats.Query, GetChats.Variables>({
+          query: getChatsQuery,
+          variables: {
+            amount: this.messagesAmount,
+          },
+          data: {
+            chats,
+          },
+        });
+      },
+    });
+  }
+
+  removeMessages(chatId: string, messages: GetChat.Messages[], messageIdsOrAll: string[] | boolean) {
+    let variables: RemoveMessages.Variables | RemoveAllMessages.Variables;
+    let ids: string[] = [];
+    let mutation: DocumentNode;
+
+    if (typeof messageIdsOrAll === 'boolean') {
+      variables = {chatId, all: messageIdsOrAll} as RemoveAllMessages.Variables;
+      ids = messages.map(message => message.id);
+      mutation = removeAllMessagesMutation;
+    } else {
+      variables = {chatId, messageIds: messageIdsOrAll} as RemoveMessages.Variables;
+      ids = messageIdsOrAll;
+      mutation = removeMessagesMutation;
+    }
+
+    return this.apollo.mutate({
+      mutation,
+      variables,
+      update: (store, { data: { removeMessages } }: {data: RemoveMessages.Mutation | RemoveAllMessages.Mutation}) => {
+        // Update the messages cache
+        {
+          // Read the data from our cache for this query.
+          const {chat} = store.readQuery<GetChat.Query, GetChat.Variables>({
+            query: getChatQuery, variables: {
+              chatId,
+            }
+          });
+          // Remove the messages (mutable)
+          removeMessages.forEach(messageId => {
+            for (const index of chat.messages.keys()) {
+              if (chat.messages[index].id === messageId) {
+                chat.messages.splice(index, 1);
+              }
+            }
+          });
+          // Write our data back to the cache.
+          store.writeQuery<GetChat.Query, GetChat.Variables>({ query: getChatQuery, data: {chat} });
+        }
+        // Update last message cache
+        {
+          // Read the data from our cache for this query.
+          const {chats} = store.readQuery<GetChats.Query, GetChats.Variables>({
+            query: getChatsQuery,
+            variables: {
+              amount: this.messagesAmount,
+            },
+          });
+          // Fix last message
+          chats.find(chat => chat.id === chatId).messages = messages
+            .filter(message => !ids.includes(message.id))
+            .sort((a, b) => Number(b.createdAt) - Number(a.createdAt)) || [];
           // Write our data back to the cache.
           store.writeQuery<GetChats.Query, GetChats.Variables>({
             query: getChatsQuery,