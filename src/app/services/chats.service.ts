Urigo/WhatsApp-Clone-Client-Angular

Step 5.2: Modify chat component and service

src/app/services/chats.service.ts
@@ -3,8 +3,9 @@ import {map} from 'rxjs/operators';
 import {Apollo} from 'apollo-angular';
 import {Injectable} from '@angular/core';
 import {getChatsQuery} from '../../graphql/getChats.query';
-import {GetChat, GetChats} from '../../types';
+import {AddMessage, GetChat, GetChats} from '../../types';
 import {getChatQuery} from '../../graphql/getChat.query';
+import {addMessageMutation} from '../../graphql/addMessage.mutation';
 
 @Injectable()
 export class ChatsService {
@@ -35,5 +36,15 @@ export class ChatsService {
     );
 
     return {query, chat$};
+  }
+
+  addMessage(chatId: string, content: string) {
+    return this.apollo.mutate({
+      mutation: addMessageMutation,
+      variables: <AddMessage.Variables>{
+        chatId,
+        content,
+      },
+    });
   }
 }

kamilkisiela/whatsapp-client-angularcli-material

src/app/services/chats.service.ts
@@ -2,8 +2,9 @@ import {map} from 'rxjs/operators';
 import {Apollo} from 'apollo-angular';
 import {Injectable} from '@angular/core';
 import {getChatsQuery} from '../../graphql/getChats.query';
-import {GetChat, GetChats} from '../../graphql';
+import {AddMessage, GetChat, GetChats} from '../../graphql';
 import {getChatQuery} from '../../graphql/getChat.query';
+import {addMessageMutation} from '../../graphql/addMessage.mutation';
 
 @Injectable()
 export class ChatsService {
@@ -38,5 +39,15 @@ export class ChatsService {
     );
 
     return {query, chat$};
+  }
+
+  addMessage(chatId: string, content: string) {
+    return this.apollo.mutate({
+      mutation: addMessageMutation,
+      variables: <AddMessage.Variables>{
+        chatId,
+        content,
+      },
+    });
   }
 }